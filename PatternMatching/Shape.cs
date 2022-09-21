ee27ccd30d3ea678ec6d306df0e625121e84944a
Papagaulo <papagaulo@outlook.com>
2022-09-21 18:17:08 -0400

    initial

---
+++ PatternMatching/Shape.cs
@@ -0,0 +1,27 @@
+﻿namespace PatternMatching
+{
+    public abstract class Shape
+    {
+        public abstract double Area { get; }
+    }
+    public interface ISquare
+    {
+        double Height { get; set; }
+        double Width { get; set; }
+    }
+
+    public class Rectangle : Shape, ISquare
+    {
+        Rectangle(double height, double width)
+        {
+            Height = height;
+            Width = width;
+
+        }
+        
+        public override double Area => Height * Width;
+
+        public double Height { get; set; }
+        public double Width { get; set; }
+    }
+}
